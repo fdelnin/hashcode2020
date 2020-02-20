5898a1dd972f9df47f7c0e06257ac0ac5cfffc57
fra <23015874+fdelnin@users.noreply.github.com>
2020-02-20 22:38:09 +0100

    final

--- solution/solution.py
+++ solution/solution.py
@@ -1,11 +1,11 @@
 import numpy
 
 class Library:
-    def __init__(self, id, n_books, signup, n_ship, all_books,books_dicts):
-        self.id = (id)
-        self.n_books = (n_books)
-        self.signup = (signup)
-        self.n_ship = (n_ship)
+    def __init__(self, id, n_books, signup, n_ship, all_books,books_dicts, remaining_days):
+        self.id = id
+        self.n_books = n_books
+        self.signup = signup
+        self.n_ship = n_ship
         self.all_books = all_books
 
         #self.my_points=self.signup+ self.n_books/self.n_ship
@@ -14,12 +14,24 @@ class Library:
             totpoints+= books_dicts[b]
             #print(books_dicts[b], b)
         #print("tot points",totpoints, n_books)
-        self.my_points= totpoints
+        giorni = self.n_books/self.n_ship
+        if self.n_books%self.n_ship!=0:
+            giorni+=1
+        giorni+= self.signup
+        if giorni > remaining_days:
+         
+            diff =giorni-remaining_days
+            giorni=remaining_days
+            totpoints=totpoints-((totpoints/self.n_books)*diff*self.n_ship)
+        if giorni ==0:
+            self.my_points = 0
+        else:
+            self.my_points = totpoints/giorni
 
     def __repr__(self):
         return "ID: {}, N_BOoks: {}, signup: {}, shiprate: {}".format(self.id, self.n_books, self.signup,self.n_ship)
 
-    def update_points(self,booktoremove):
+    def update_points(self,booktoremove, remaining_days):
        # print(self.all_books)
         for b in self.all_books:
             if b in booktoremove:
@@ -30,8 +42,18 @@ class Library:
             totpoints+= books_dicts[b]
             #print(books_dicts[b], b)
        # print("tot points",totpoints, self.n_books)
-        self.my_points= totpoints
-        self.my_points=totpoints
+        giorni = self.n_books/self.n_ship
+        if self.n_books%self.n_ship!=0:
+            giorni+=1
+        giorni+= self.signup
+        if giorni > remaining_days:
+            diff =giorni-remaining_days
+            giorni=remaining_days
+            totpoints=totpoints-((totpoints/self.n_books)*diff*self.n_ship)
+        if giorni ==0:
+            self.my_points = 0
+        else:
+            self.my_points = totpoints/giorni
 
 def check_books(already_sent, new_books):
     #print("called check books")
@@ -45,30 +67,41 @@ def check_books(already_sent, new_books):
 def used_days(total_days, libs):
    # print("called used days")
     remaining = total_days
-    books_sent = []
+    
     libs_considered = []
-    all=0
-    while all< len(libs) and remaining>0:
+
+    
+    while libs!=[] and remaining>0:
+        books_sent = []
+       # print(libs)
         l=libs[0]
+        reorder=False
         if l.signup <= remaining:
            
-            books_to_send = check_books(books_sent, l.all_books)
-            if books_to_send !=[]:
+            #books_to_send = check_books(books_sent, l.all_books)
+            if l.all_books !=[]:
+                
                 remaining-=l.signup
-                libs_considered.append((l.id, books_to_send))
-                books_sent.extend(books_to_send)
-                libs.remove(l)
-                for i in libs:
-                    i.update_points(books_sent)
-                libs.sort(key=lambda x: x.my_points, reverse=True)
-                    
-        all+=1
+                if remaining>0:
+                   reorder=True
+                libs_considered.append((l.id, l.all_books))
+                books_sent=l.all_books
             
+        libs.remove(l)
+        if reorder:
+            for i in libs:
+                i.update_points(books_sent, remaining)
+            libs.sort(key=lambda x: x.my_points, reverse=True)
+                       
 
     return libs_considered
 
 #f = open('../input/a_example.txt', 'r')
-all_files= ['a_example','b_read_on', 'c_incunabula', 'd_tough_choices','e_so_many_books','f_libraries_of_the_world']
+all_files= [#
+#'b_read_on', 'c_incunabula']#, 
+#'d_tough_choices', 
+#'e_so_many_books',
+'f_libraries_of_the_world']#,'b_read_on', 'c_incunabula'] #
 for a in all_files:
     filename = '../input/'+a+'.txt'
     print(filename)
@@ -108,7 +141,7 @@ for a in all_files:
     #print(len(input_n))
     for i  in range(0, len(input_n)-1, 2):
     #  print(i, input_n[i])
-        lib = Library(count, input_n[i][0], input_n[i][1], input_n[i][2], input_n[i+1],books_dicts)
+        lib = Library(count, input_n[i][0], input_n[i][1], input_n[i][2], input_n[i+1],books_dicts, tot_days)
         libs.append(lib)
         count+=1
     
@@ -116,7 +149,7 @@ for a in all_files:
     #print(libs)
     #sort libraries by sign up time or n ship?
     libs.sort(key=lambda x: x.my_points, reverse=True)
-    print(libs[0], "last: ", libs[-1])
+    #print(libs[0], "last: ", libs[-1])
     #print(libs)
     libs_output = used_days(tot_days, libs)
 
